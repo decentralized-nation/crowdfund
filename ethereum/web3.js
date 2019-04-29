5a8905ea17ebb0c85c799d82fcb07a1cab435f5d
Devin Herath <devdhera@gmail.com>
2019-04-29 20:15:11 +0530

    Fixed #3 by injecting providers on sever

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -1,5 +1,15 @@
 import Web3 from 'web3';
+import keys from '../config/keys';
 
-const web3 = new Web3(window.web3.currentProvider);
+let web3;
+
+if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
+    web3 = new Web3(window.web3.currentProvider);
+} else {
+    const provider = new Web3.providers.HttpProvider(
+        keys.endPoint
+    );
+    web3 = new Web3(provider);
+}
 
 export default web3;
